6a636f3aaeb2cfa91676b295d9da2be0dc87e9da
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-04-25 18:28:53 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 use actix_web::middleware::Logger;
 use env_logger::Env;
+use tracing_actix_web::TracingLogger;
+use simplelog::{CombinedLogger, Config, LevelFilter, WriteLogger};
+use std::fs::File;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -15,13 +18,36 @@ async fn hi() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
-   env_logger::init_from_env(Env::default().default_filter_or("info"));
+    env_logger::init_from_env(Env::default().default_filter_or("trace")); 
+
+    /*
+    let log_file = File::create("./app.log").unwrap();
+
+
+
+    CombinedLogger::init(
+        vec![
+            // Write logs to file
+            WriteLogger::new(
+                LevelFilter::Debug,
+                Config::default(),
+                log_file,
+            ),
+
+            // Write logs to console
+            WriteLogger::new(LevelFilter::Debug, Config::default(), std::io::stdout()),
+        ]
+    ).unwrap();*/
+
+   
+
     HttpServer::new(|| {
         App::new()
+            .wrap(TracingLogger::default())
             .service(hello)
             .service(hi)
-            .wrap(Logger::default())
-            .wrap(Logger::new("%a %{User-Agent}i"))
+            //.wrap(Logger::default())
+            
             
             
     })
